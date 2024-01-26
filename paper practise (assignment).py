106e266f20f26100853df6d1b60fdf63b5826514
Irfan Ali <151863237+Irfan-Ali01@users.noreply.github.com>
2024-01-26 13:26:38 +0500

    Add files via upload

---
+++ paper practise (assignment).py
@@ -0,0 +1,20 @@
+def recommend_crop(season,temperature,rainfall,soil_type):
+    if season=='winter' and temperature>30 and temperature<=35 and rainfall>400:
+        print('Not Suitable')
+    if season=='summer' and temperature>30 and temperature<=35 and rainfall>400:
+        print('Crop; Rice')
+    if season=='winter' and temperature>35 and soil_type=='sandy':
+        print('Not Suitable')
+    if season=='summer' and temperature>35 and soil_type=='sandy':
+        print('Crop; Cotton')
+    if temperature>=25 and temperature<=30:
+        print('Crop; Bajra')
+print('~'*30)
+print("   Crop Recommendation System")
+print('~'*30)
+print()
+season=str(input('Enter Season; ')).lower()
+temperature=eval(input('Enter the Temp; '))
+rainfall=int(input('Enter Rainfall; '))
+soil_type=str(input('Enter type Of Soil; ')).lower()
+recommend_crop(season,temperature,rainfall,soil_type)
